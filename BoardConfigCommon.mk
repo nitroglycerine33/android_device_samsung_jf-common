6bf1d5e797f75d283fc5743de6a83bd356ac5c3c
nitroglycerine33 <nitroglycerine33@gmail.com>
2013-08-20 11:08:51 -0500

    Update audio and add bluetooth parameters

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,6 +91,7 @@ BOARD_SEPOLICY_UNION += \
 	file_contexts \
 	property_contexts \
 	te_macros \
+	bluetooth_loader.te \
 	bridge.te \
 	camera.te \
 	conn_init.te \
@@ -99,8 +100,8 @@ BOARD_SEPOLICY_UNION += \
 	domain.te \
 	drmserver.te \
 	file.te \
-	kickstart.te \
 	init.te \
+	kickstart.te \
 	mediaserver.te \
 	mpdecision.te \
 	netmgrd.te \
